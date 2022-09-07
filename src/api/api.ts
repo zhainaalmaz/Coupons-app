a1db9f5e8061e723e06568a55e6d42d5e0bb017e
firdavsnurpaizov <firdavs.nurpaizov.ss@gmail.com>
2022-09-07 16:14:58 +0600

    add authslice

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,22 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
-    return new Promise<{ data: number }>((resolve) =>
-      setTimeout(() => resolve({ data: amount 
-      }), 500)
-    );
-  }
-  
+// export function fetchCount(amount = 1) {
+//     return new Promise<{ data: number }>((resolve) =>
+//       setTimeout(() => resolve({ data: amount 
+//       }), 500)
+//     );
+//   }
+
+  // import * as axios from "axios"
+
+  // const instance = axios.create({
+  //     baseURL: 'http://185.178.44.117/api/v1/',
+  // });
+
+
+  // export function auth() {
+  //   return instance.get()
+  //     .then((response) => {
+  //       return response.data
+  //     })
+
+  // }
--- src/store/slices/authSlice/index.ts
+++ src/store/slices/authSlice/index.ts
@@ -1,8 +1,6 @@
 import { FormValues } from "../../../components/auth/signup/Signup";
-
-
-
-
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import { auth } from "../../../api/api";
 
 
 const initialState: FormValues = {
@@ -12,3 +10,29 @@ const initialState: FormValues = {
     password: '',
     password_confirm: '',
 };
+
+
+const authThunk = createAsyncThunk(
+    'signup',
+    async () => {
+        try {
+            // const res = await auth()
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+)
+
+
+const authSlice = createSlice({
+    name: "signup",
+    initialState,
+    reducers: {},
+    extraReducers: {},
+
+})
+
+
+export const signupAction = authSlice.actions;
+export default authSlice.reducer;
