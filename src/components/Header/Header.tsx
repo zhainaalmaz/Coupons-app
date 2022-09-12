7b4273a3260e7e90fb253f5dec979063dce76696
makhabatabd <makhabat9898@gmail.com>
2022-09-12 15:20:26 +0600

    header favorites and user

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,6 +9,7 @@ import couponIcon from "../../assets/headerIcons/coupon.svg";
 import logOut from "../../assets/headerIcons/log-in.svg";
 import Search from "../Search/Search";
 import BurgerIcon from "../../assets/headerIcons/burgerIcon.png";
+import redHeartIcon from "../../assets/headerIcons/red-heart.png";
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import { getContactAsync } from "../../store/slices/contactSlice";
 import { Link } from "react-router-dom";
@@ -17,9 +18,15 @@ const Header = () => {
   const [open, setOpen] = React.useState<boolean>(false);
   const dispatch = useAppDispatch();
   const state = useAppSelector((state) => state.contacts);
+  const favoriteCoupons = useAppSelector(
+    (state) => state.favorite.favoriteCoupons
+  );
+  const userName = localStorage.getItem("user");
+  console.log(userName);
   useEffect(() => {
     dispatch(getContactAsync());
   }, []);
+
   return (
     <header>
       <div className={styles.headerLines}>
@@ -67,26 +74,46 @@ const Header = () => {
             <Search />
             <Hidden mdDown>
               <div className={styles.headerExtraInfo}>
-                <Link to="favorites">
-                  <div className={styles.iconWrapper}>
-                    <img src={heartIcon} alt="heart-icon" />
-                    <span className={styles.headerOption}>Избранное</span>
-                  </div>
-                </Link>
+                {favoriteCoupons.length > 0 ? (
+                  <Link to="favorites">
+                    <div className={styles.iconWrapper}>
+                      <img
+                        width="23px"
+                        src={redHeartIcon}
+                        alt="red-heart-icon"
+                      />
+                      <span className={styles.headerOption}>Избранное</span>
+                    </div>
+                  </Link>
+                ) : (
+                  <Link to="favorites">
+                    <div className={styles.iconWrapper}>
+                      <img width="23px" src={heartIcon} alt="heart-icon" />
+                      <span className={styles.headerOption}>Избранное</span>
+                    </div>
+                  </Link>
+                )}
                 <Link to="my-coupons">
                   <div className={styles.iconWrapper}>
                     <img src={couponIcon} alt="coupon-icon" />
                     <span className={styles.headerOption}>Мои купоны</span>
                   </div>
                 </Link>
-
-                <Link to="sign-up">
-                  {" "}
-                  <div className={styles.iconWrapper}>
-                    <img src={logOut} alt="log-out" />
-                    <span className={styles.headerOptionLog}>Войти</span>
-                  </div>
-                </Link>
+                {userName ? (
+                  <Link to="/">
+                    <div className={styles.iconWrapperUser}>
+                      <img src={logOut} alt="log-out" />
+                      <span className={styles.user}>{userName}</span>
+                    </div>
+                  </Link>
+                ) : (
+                  <Link to="sign-up">
+                    <div className={styles.iconWrapper}>
+                      <img src={logOut} alt="log-out" />
+                      <span className={styles.headerOptionLog}>Войти</span>
+                    </div>
+                  </Link>
+                )}
               </div>
             </Hidden>
           </div>
@@ -126,22 +153,50 @@ const Header = () => {
               </div>
             </div>
             <div>
-              <div className={styles.iconWrapper}>
-                <img src={heartIcon} alt="heart-icon" />
-                <p className={styles.headerSelected}>Избранное</p>
-              </div>
-              <div className={styles.iconWrapper}>
-                <img
-                  style={{
-                    width: "20px",
-                    marginRight: "-1px",
-                    marginTop: "-3px",
-                  }}
-                  src={couponIcon}
-                  alt="coupon"
-                />
-                <p>Мои купоны</p>
-              </div>
+              {favoriteCoupons.length > 0 ? (
+                <Link to="favorites">
+                  <div className={styles.smallIconWrapper}>
+                    <img width="23px" src={redHeartIcon} alt="red-heart-icon" />
+                    <span className={styles.burgerSpans}>Избранное</span>
+                  </div>
+                </Link>
+              ) : (
+                <Link to="favorites">
+                  <div className={styles.smallIconWrapper}>
+                    <img width="23px" src={heartIcon} alt="heart-icon" />
+                    <span className={styles.burgerSpans}>Избранное</span>
+                  </div>
+                </Link>
+              )}
+              <Link to="/">
+                <div className={styles.smallIconWrapper}>
+                  <img
+                    style={{
+                      width: "20px",
+                      marginRight: "-1px",
+                      marginTop: "-3px",
+                    }}
+                    src={couponIcon}
+                    alt="coupon"
+                  />
+                  <span className={styles.burgerSpans}>Мои купоны</span>
+                </div>
+              </Link>
+              {userName ? (
+                <Link to="/">
+                  <div className={styles.smallIconWrapper}>
+                    <img src={logOut} alt="log-out" />
+                    <span className={styles.user}>{userName}</span>
+                  </div>
+                </Link>
+              ) : (
+                <Link to="sign-up">
+                  <div className={styles.smallIconWrapper}>
+                    <img src={logOut} alt="log-out" />
+                    <span className={styles.burgerSpans}>Войти</span>
+                  </div>
+                </Link>
+              )}
             </div>
           </div>
           <div className={styles.burgerBottom}>
